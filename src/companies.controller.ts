72a09883f6ccdb7956b3eb8fd7a9315cfee100ce
Umid Almatov <almatov.us@gmail.com>
2017-10-13 20:33:52 +0500

    Created company model and controller adjusted to use that model

--- src/companies.controller.ts
+++ src/companies.controller.ts
@@ -1,10 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
+import { Company } from './company';
 
 @Controller('companies')
 export class CompaniesController {
 
+  private companies = [
+    new Company('Coke', 'Soda', 'Washington DC'),
+    new Company('Apple', 'Computes', 'New York'),
+    new Company('Tesla', 'Cars', 'Alabama')
+  ];
+
   @Get()
   getCompanies() {
-    return ['Coke', 'Apple', 'Tesla'];
+    return this.companies;
   }
 }
---
+++ src/company.ts
@@ -0,0 +1,9 @@
+export class Company {
+
+  constructor(
+    public name: string,
+    public industry: string,
+    public address: string
+  ) {}
+
+}
